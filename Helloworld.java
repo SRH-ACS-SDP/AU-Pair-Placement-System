dd213752d2bf50d6ce52d6b8b345833f6331a9a9
jagtarsinghtandon <39553429+jagtarsinghtandon@users.noreply.github.com>
2018-05-27 22:48:26 +0200

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,14 @@
+// Exercise 1 : Helloworld
+
+public class Helloworld 
+{
+
+	public static void main(String [] args)
+	{
+
+		System.out.println("My name is Jagtar Singh Tandon" + "\n" 
+		+ "I will work hard and complete my masters studies" + "\n" 
+		+ "I will not copy the assignments, exercises or the final mini-project" + "\n" 
+		+ "Quote -> \"I can cheat and copy the assignments but this will make an inept programmer\"");
+	}
+}
